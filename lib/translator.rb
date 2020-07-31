6d3aeaa75a1d7decfd0e1ad865c2d8c6db2a2a39
Islom Mashanlo <islom.mashanlo@gmail.com>
2020-07-31 19:51:56 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -26,7 +26,7 @@ def get_japanese_emoticon(locat, emoticon)
   response = nil
   emoticon_hash.each do |meaning, language|
     if language[:english] == emoticon
-      response == meaning[language][:japanese]
+      response == language[:japanese]
     end
   end
   if response == nil
