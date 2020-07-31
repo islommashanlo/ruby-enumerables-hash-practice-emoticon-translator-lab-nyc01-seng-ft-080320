069bdef7b4bdef6893281d740832676c74e9b1a0
Islom Mashanlo <islom.mashanlo@gmail.com>
2020-07-31 19:42:34 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,39 @@
 # require modules here
+require 'yaml'
+require 'pry'
 
-def load_library
-  # code goes here
+def load_library(locat)
+  emoticons = YAML.load_file(locat)
+  emoticon_hash = {}
+
+#method to organize the hash
+    emoticons.each do |meaning, emoticon_set|
+        english_emote = emoticon_set[0]
+        japanese_emote = emoticon_set[1]
+        emoticon_hash[meaning] = {
+          english: {},
+          japanese: {}
+        }
+        emoticon_hash[meaning][:english] = english_emote
+        emoticon_hash[meaning][:japanese] = japanese_emote
+      end
+    emoticon_hash
 end
 
-def get_japanese_emoticon
+def get_japanese_emoticon(locat, emoticon)
   # code goes here
+
 end
 
-def get_english_meaning
+def get_english_meaning(locat, emoticon)
   # code goes here
+  emoticons = YAML.load_file(locat)
+  emoticon_hash = load_library(locat)
+  response = nil
+  emoticon_hash.each do |meaning, emoticon_set|
+    if emoticon == emoticon_set
+      then response = meaning
+    else return "Try again"
+    end
+  end
 end
